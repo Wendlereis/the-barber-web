1c87a8dfd328cd74f89f58ef361a82ffba4c499c
Wendler Eis <wendler_eis@hotmail.com>
2020-08-06 21:48:05 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import GlobalStyles from "./styles/Global";
+
 const App: React.FC = () => {
   return (
     <>
       <h1>The Barber</h1>
+      <GlobalStyles />
     </>
   );
 };
