63d94cf8c8f7e92ba03b8f7be8c1e8a9622ce901
Wendler Eis <wendler_eis@hotmail.com>
2020-08-09 00:22:49 -0300

    feat: apply input and button components

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -27,41 +27,6 @@ export const Content = styled.section`
       margin-bottom: 24px;
     }
 
-    input {
-      background-color: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background-color: #ff9000;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      font-weight: 500;
-      line-height: 21px;
-      width: 100%;
-      height: 56px;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background-color: ${shade(0.2, "#FF9000")};
-      }
-    }
-
     > a {
       color: #f4ede8;
       display: block;
@@ -76,7 +41,7 @@ export const Content = styled.section`
   }
 
   > a {
-    color: #FF9000;
+    color: #ff9000;
     display: flex;
     align-items: center;
     margin-top: 24px;
